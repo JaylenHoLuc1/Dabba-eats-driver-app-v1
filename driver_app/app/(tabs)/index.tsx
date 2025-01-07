a040c978dbc188e7dde5887a2afdd76064c13134
JaylenHoLuc1 <luc.jaylen@dabba-eats.com>
2025-01-07 00:45:39 +0000

    order items for drivers template

--- driver_app/app/(tabs)/index.tsx
+++ driver_app/app/(tabs)/index.tsx
@@ -1,25 +1,83 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import orders from '../../assets/data/orders.json';
+import { Entypo } from '@expo/vector-icons';
+//dummy data format, could be used in production with some minor tweaks
+type Restaurant = {
+  id: string;
+  name: string;
+  image: string;
+  deliveryFee: number;
+  minDeliveryTime: number;
+  maxDeliveryTime: number;
+  rating: number;
+  address: string;
+  lat: number;
+  lng: number;
+  createdAt: string;
+  updatedAt: string;
+};
 
+type User = {
+  id: string;
+  name: string;
+  address: string;
+  lat: number;
+  lng: number;
+};
+
+type Order = {
+  id: string;
+  userID: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  orderRestaurantId: string;
+  Restaurant: Restaurant;
+  User: User;
+};
+
+type OrderArray = Order[];
+
+const order : Order = orders[0];
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#D4C2B4' }}
+      headerBackgroundColor={{ light: '#D4C2B4', dark: '#C2A9A1' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/dabba.png')}
+          style={styles.dabbaLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+      <ThemedView >
+        <ThemedText type="title">Driver app</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+
+      <ThemedView style={styles.orderOuter}>
+        <ThemedView style={styles.orderInner}>
+          <Image source={{uri: order.Restaurant.image}} style={styles.orderImage}></Image>
+        
+          <ThemedView style={{marginLeft: 10, flex: 2, paddingVertical: 5}}>
+            <ThemedText type="title">{order.Restaurant.name}</ThemedText>
+            <ThemedText style={styles.subtext}  type="subtitle">{order.Restaurant.address}</ThemedText>
+            <ThemedText style={{marginTop: 10}} type="subtitle">Delivery Details:</ThemedText>
+            <ThemedText style={styles.subtext}  type="subtitle">{order.User.name}</ThemedText>
+            <ThemedText style={styles.subtext}  type="subtitle">{order.User.address}</ThemedText>
+          </ThemedView>
+
+          <ThemedView style={{padding: 5, backgroundColor: '#3FC060', borderBottomRightRadius: 10, borderTopRightRadius: 10, alignItems : 'center', justifyContent: 'center'}}>
+            <Entypo name="check" size={30} color="white" style={{marginLeft: 'auto'}}></Entypo>
+          </ThemedView>
+
+        </ThemedView>
+      </ThemedView>
+      {/* <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
@@ -33,23 +91,7 @@ export default function HomeScreen() {
           </ThemedText>{' '}
           to open developer tools.
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      </ThemedView> */}
     </ParallaxScrollView>
   );
 }
@@ -60,16 +102,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
+  subtext: {
+    color: 'grey'
+  },
+  orderOuter: {
     gap: 8,
     marginBottom: 8,
     
   },
-  reactLogo: {
+  orderInner : {
+    flexDirection: 'row',
+    borderColor: '#D4C2B4',
+    borderWidth : 3,
+    borderRadius : 14,
+    margin: 10,
+
+    
+
+  }
+  ,
+  dabbaLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
+  orderImage: {
+    width: '30%',
+    height: '100%',
+    borderRadius : 10
+  }
 });
