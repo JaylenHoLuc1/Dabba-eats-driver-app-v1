9582df0efe701b456f704d663ccba16d54228833
JaylenHoLuc1 <luc.jaylen@dabba-eats.com>
2025-01-08 01:18:33 +0000

    started nav safety push

--- driver_app/screens/OrderDelivery/index.tsx
+++ driver_app/screens/OrderDelivery/index.tsx
@@ -6,6 +6,7 @@ import { useMemo, useRef } from 'react';
 import { FontAwesome5, Fontisto } from '@expo/vector-icons';
 import BottomSheet from '@gorhom/bottom-sheet';
 import orders from '../../assets/data/orders.json';
+import styles from './styles';
 
 const order = orders[0]
 
@@ -30,7 +31,7 @@ const OrderDelivery = () => {
                     <ThemedText style={{color : 'black', fontSize : 25, letterSpacing : 1}}>5 Km</ThemedText>   
                 </View>
                 <View style={styles.info} >
-                <ThemedText style={styles.titles}>{order.Restaurant.name}</ThemedText>
+                    <ThemedText style={styles.titles}>{order.Restaurant.name}</ThemedText>
                     <View style={{flexDirection : 'row', marginBottom: 10,}}>
                         <Fontisto name="shopping-store" size={22} color='#C2A9A1' />
                         <ThemedText style={styles.details}>{order.Restaurant.address}</ThemedText>                    
@@ -44,34 +45,15 @@ const OrderDelivery = () => {
                         <ThemedText style={styles.details} >dummy food</ThemedText>
                     </View>
                 </View>
+                <View style={{borderRadius: 15, backgroundColor: "#C2A9A1",marginBottom : 45, marginTop: 'auto', marginVertical: 20, marginHorizontal: 10}}>
+                    <ThemedText style={{color: 'white', padding: 10, fontWeight: '500', textAlign: 'center'}}>Accept Order</ThemedText>
+
+                </View>
             </BottomSheet>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-
-    Container : {
-      backgroundColor: '#C2A9A1'
-  
-    },
-    details : {
-        color : 'black', fontSize : 15, letterSpacing : 1, marginLeft : 15
-
-    },
-    titles : {
-        color : 'black', fontSize : 20, letterSpacing : 1, 
-        fontWeight: '500', paddingVertical: 20
-    },
-
-    foods : {
-        borderTopWidth: 1, borderColor: '#C2A9A1'
-    },
-    info : {
-        paddingHorizontal : 20,
-    }
-    
-  });
 
 export default OrderDelivery;
 
---
+++ driver_app/screens/OrderDelivery/styles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export default  StyleSheet.create({
+
+    Container : {
+      backgroundColor: '#C2A9A1'
+  
+    },
+    details : {
+        color : 'black', fontSize : 15, letterSpacing : 1, marginLeft : 15
+
+    },
+    titles : {
+        color : 'black', fontSize : 20, letterSpacing : 1, 
+        fontWeight: '500', paddingVertical: 20
+    },
+
+    foods : {
+        borderTopWidth: 1, borderColor: '#C2A9A1', paddingTop: 20
+    },
+    info : {
+        paddingHorizontal : 20,
+    }
+    
+  });
