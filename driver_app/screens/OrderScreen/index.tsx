b1cbdf6393fc338ed46345cbbc3f2e1d41ca4fbc
JaylenHoLuc1 <luc.jaylen@dabba-eats.com>
2025-01-08 23:29:08 +0000

    map compatability blocker

--- driver_app/screens/OrderScreen/index.tsx
+++ driver_app/screens/OrderScreen/index.tsx
@@ -11,16 +11,52 @@ import OrderItem from '../../components/OrderItem';
 import { Order } from "@/app/types/types";
 import { FlatList } from 'react-native-gesture-handler';
 import BottomSheet from "@gorhom/bottom-sheet";
+//import MapView from 'react-native-maps';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { Dimensions } from 'react-native';
+import React from 'react';
 
 //dummy data format, could be used in production with some minor tweaks
 export default function OrdersScreen() {
     const bottomSheetRef = useRef<BottomSheet>(null);
 
     const snapPoints = useMemo(() => ["12%", "95%"], [])
-    
+    const isWeb = Platform.OS === 'web';
+    const center = {
+      lat: 37.7749, // Example latitude (San Francisco)
+      lng: -122.4194, // Example longitude (San Francisco)
+    };
   return (
    
         <ThemedView style={{backgroundColor : '#C2A9A1'}} >
+          
+            {isWeb? (
+              <LoadScript googleMapsApiKey={process.env.GOOGLE_API_KEY as string}>
+                <GoogleMap
+                  mapContainerStyle={{
+                    height : Dimensions.get('window').height, 
+                    width: Dimensions.get('window').width
+                  }}
+                  center={center}
+                  zoom={10}
+                ></GoogleMap>
+             </LoadScript>
+            ) : (<></>)
+            //  MapView && (
+            //     <React.Fragment>
+            //       <MapView 
+            
+            //         style={{
+            //           height : Dimensions.get('window').height, 
+            //           width: Dimensions.get('window').width
+            //         }}
+
+            //     />
+            // </React.Fragment>
+            // )
+            }
+
+          
             <BottomSheet  handleIndicatorStyle={{backgroundColor : '#C2A9A1', width : 100}}  ref={bottomSheetRef} snapPoints={["20%", "90%"]}>
                 <ThemedView style={{flex : 1, alignItems : 'center'}}>
                   <ThemedText style={{fontSize: 20, fontWeight : 600, paddingBottom : 5}}>You are Online!</ThemedText>
